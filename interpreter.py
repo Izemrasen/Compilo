a19911bf25a9cbf8490c3a0bc1209c37db6b3f3a
JeanMichou293 <JeanMichou293@users.noreply.github.com>
2018-04-17 19:33:24 +0200

    L'interpréteur commence à claquer

--- interpreter.py
+++ interpreter.py
@@ -4,88 +4,98 @@ import os
 import sys
 import argparse
 
-ip = 0							# Instruction pointer
-reg = [0 for x in range(16)]	# Registers
-reg_map = { "R0": 0, "R1": 1, "R2": 2, "R3": 3}
-mem = [0 for x in range(1024)]	# Data memory
+ip = 0								# Instruction pointer
+reg = dict(zip(						# Registers
+ ['R' + str(x) for x in range(16)],
+ [0 for x in range(16)]))
+mem = [0 for i in range(1024)]		# Data memory
 
-def get_reg(string_reg):
-	global reg
-	index = reg_map[string_reg]
-	return reg[index]
+def get_reg(str_reg):
+	return reg[str_reg]
 
-def set_reg(string_reg, val):
-	global reg
-	index = reg_map[string_reg]
-	reg[index] = val
+def set_reg(str_reg, val):
+	reg[str_reg] = val
 
 def debug(instrs):
 	print(reg)
-	print(mem)
-	i = instrs[ip]
-	(op, a, b, c) = i.split('\t')
-	print(ip, ":", op, a, b, c)
+	#print(mem)
+	#print(instrs[ip])
 
 def process(instrs):
 	global ip
-#	instr = instr.replace('\n', '')
 	i = instrs[ip]
-	(op, a, b, c) = i.split('\t')
 	
-	if op == "JMP":
+	# Handle mssing arguments
+	try:
+		s = i.split('\t', maxsplit=4)
+		op = s[0]
+		a = s[1]
+		b = s[2]
+		c = s[3]
+	except IndexError:
+		#print("Warning on l. %d: Possibly missing arguments" % ip)
+		pass
+	
+	# Process instructions
+	if op[:1] == '#':	# Comments
+		pass
+	elif op == 'JMP':
 		a = int(a)
 		ip += a
-	elif op == "JMPC" and get_reg(b) == 0:
+	elif op == 'JMPC' and get_reg(b) == 0:
 		a = int(a)
 		ip += a
-	elif op == "AFC":
+	elif op == 'AFC':
 		b = int(b)
 		set_reg(a, b)
-	elif op == "LOAD":
+	elif op == 'LOAD':
 		b = int(b)
 		set_reg(a, mem[b])
-	elif op == "STORE":
+	elif op == 'STORE':
 		a = int(a)
 		mem[a] = get_reg(b)
-	elif op == "EQU":
+	elif op == 'EQU':
 		val = 1 if get_reg(b) == get_reg(c) else 0
 		set_reg(a, val)
-	elif op == "INF":
+	elif op == 'INF':
 		val = 1 if get_reg(b) < get_reg(c) else 0
 		set_reg(a, val)
-	elif op == "INFE":
+	elif op == 'INFE':
 		val = 1 if get_reg(b) <= get_reg(c) else 0
 		set_reg(a, val)
-	elif op == "SUP":
+	elif op == 'SUP':
 		val = 1 if get_reg(b) > get_reg(c) else 0
 		set_reg(a, val)
-	elif op == "SUPE":
+	elif op == 'SUPE':
 		val = 1 if get_reg(b) >= get_reg(c) else 0
 		set_reg(a, val)
-	debug(instrs)
+	elif op == 'PRINT':	# Easter egg
+		char_ptr = int(a)
+		byte = mem[char_ptr]
+		while byte != 0:
+			byte = mem[char_ptr]
+			print(chr(byte), end='', flush=True)
+			char_ptr += 1
+			byte = mem[char_ptr]
+	#debug(instrs)
 	ip += 1
 
 def main():
 	if len(sys.argv) < 2:
 		print("Missing arguments")
-		exit(1)
+		sys.exit(1)
 	
 	# Open assembly source
 	filename = sys.argv[1]
 	f = open(filename)
 	instrs = f.read().split('\n')
-	# Debug
-	cnt = 0
-	for i in instrs:
-		print(cnt, ":", i)
-		cnt += 1
-	print('\n\n')
 	
 	# Process instructions
 	instr_count = len(instrs)
-	
 	while ip < instr_count:
 		process(instrs)
+	
+	debug(instrs)
 	
 	f.close()
 	sys.exit(0)
